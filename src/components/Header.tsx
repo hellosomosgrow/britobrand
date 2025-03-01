218558fe15dc3891ac8955a04153cc4efddcec25
andrenavas <andreynanavas@gmail.com>
2025-03-01 14:05:21 -0500

    fix header padding

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 export default function Header() {
   return (
-    <header className="w-full bg-[#1E1E1E] text-white px-5 py-2 rounded-[10px]">
+    <header className="w-full bg-[#1E1E1E] text-white py-2 rounded-[10px]">
      
         {/* Versión Desktop: Logo + Menú */}
         {/* <h1 className="hidden md:block text-2xl font-cuteFont">BRITO BRAND</h1> */}
         {/* Menú (Mobile & Desktop) */}
-        <nav className="text-lg font-funnelSans flex w-full items-center justify-between">
+        <nav className="text-lg font-funnelSans flex w-full items-center justify-between px-4">
           <a href="/servicios" className="px-4 md:px-2">
             Servicios 
           </a>
