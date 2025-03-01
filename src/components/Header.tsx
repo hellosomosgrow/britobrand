aff66a8b44c52b415f1ced4b64628fa8ea5756ea
andrenavas <andreynanavas@gmail.com>
2025-03-01 13:54:42 -0500

    fix nav styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,12 +3,11 @@ import React from 'react';
 export default function Header() {
   return (
     <header className="w-full bg-[#1E1E1E] text-white px-5 py-2 rounded-[10px]">
-      <div className="container mx-auto flex justify-between items-center">
+     
         {/* Versión Desktop: Logo + Menú */}
-        <h1 className="hidden md:block text-2xl font-cuteFont">BRITO BRAND</h1>
-
+        {/* <h1 className="hidden md:block text-2xl font-cuteFont">BRITO BRAND</h1> */}
         {/* Menú (Mobile & Desktop) */}
-        <nav className="flex w-full md:w-auto justify-between items-center text-lg font-funnelSans">
+        <nav className="text-lg font-funnelSans flex w-full items-center justify-between">
           <a href="/servicios" className="px-4 md:px-2">
             Servicios 
           </a>
@@ -21,7 +20,7 @@ export default function Header() {
             Contacto
           </a>
         </nav>
-      </div>
+     
     </header>
   );
 }
