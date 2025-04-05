e2c41468b4713588ce8e8a468de04d30aee1d319
andrenavas <andreynanavas@gmail.com>
2025-04-05 12:55:31 -0500

    update: navigate to project details

--- src/components/ProjectsPreview.tsx
+++ src/components/ProjectsPreview.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ProjectPreviewCard from "./ProjectPreviewCard";
 import { projectsPreview, projectsLarge } from "@/data/projectsData";
+import { useNavigate } from "react-router-dom";
 
 const ProjectsPreview = () => {
   return (
@@ -17,9 +18,13 @@ const ProjectsPreview = () => {
 };
 
 const ProjectPreviewLargeCard = ({ project }: { project: { title: string; image: string, type: string} }) => {
+  const navigate = useNavigate();
 
+  const handleViewMore = () => {
+    navigate(`/proyectos/${project.title}`);
+  };
   return (
-    <div className="text-[#1E1E1E] rounded-lg">
+    <div className="text-[#1E1E1E] rounded-lg" onClick={handleViewMore}>
       <img src={project.image} alt={project.title} className="w-full h-auto object-cover rounded-[10px]" />
       <div className="flex justify-between p-1">
         <h3 className="text-lg font-semibold">{project.title}</h3>
