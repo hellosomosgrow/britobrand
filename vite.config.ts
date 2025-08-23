3f9a38c18edc089d67095decc69dcd3d604073c5
andrenavas <andreynanavas@gmail.com>
2025-08-23 16:31:32 -0500

    fix reload vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
-import path from 'path';
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': '/src',
     },
   },
   build: {
     outDir: 'dist',
-    rollupOptions: {
-      input: path.resolve(__dirname, 'index.html'),
-    },
   },
-  base: './',
+  base: '/',
 });
